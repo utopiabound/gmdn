49055f00c09595a6ff4b72be8c015f5535c3c093
Nathaniel Clark <Nathaniel.Clark@misrule.us>
2023-03-23 12:42:13 -0400

    Add convergence, and updates
    Switch structopt to clap.
    Add convergence checking
    
    Signed-off-by: Nathaniel Clark <Nathaniel.Clark@misrule.us>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 // Copyright (c) 2021 Nathaniel Clark
 
-use structopt::StructOpt;
+use clap::Parser;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "gmdn", about = "Geothmetic Meandian")]
+/// Geothmetic Meandian
+///
+/// [Arithmetic Mean, Geometric Mean, Median]
+#[derive(Debug, Parser)]
+#[clap(name = "gmdn", about = "Geothmetic Meandian")]
 struct Opt {
-    /// Iterations
-    #[structopt(short, long, default_value = "1")]
-    iterations: u32,
+    /// Maximum Iterations
+    #[clap(short, long)]
+    iterations: Option<u32>,
+
+    /// Matching decimal places
+    #[clap(short, long, default_value = "4")]
+    precision: u8,
 
     /// Enable debug
-    #[structopt(short, long)]
+    #[clap(short, long)]
     debug: bool,
 
+    /// Print info about final answer
+    #[clap(short, long)]
+    verbose: bool,
+
     /// Floating point numbers run progression on
+    #[clap(required = true)]
     numbers: Vec<f64>,
 }
 
@@ -26,7 +38,7 @@ fn amean(v: &Vec<f64>) -> f64 {
 fn gmean(v: &Vec<f64>) -> f64 {
     let n = v.len() as f64;
     assert!(n > 0.0);
-    v.iter().fold(1.0, |acc, x| acc * x).powf(1.0/n)
+    v.iter().fold(1.0, |acc, x| acc * x).powf(1.0 / n)
 }
 
 fn meadian(v: &Vec<f64>) -> f64 {
@@ -35,7 +47,7 @@ fn meadian(v: &Vec<f64>) -> f64 {
     let h = (n + n % 2) / 2;
     let mut v2 = v.clone();
     v2.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
-    *v2.get(h-1).unwrap()
+    *v2.get(h - 1).unwrap()
 }
 
 fn f(v: &Vec<f64>) -> Vec<f64> {
@@ -43,14 +55,29 @@ fn f(v: &Vec<f64>) -> Vec<f64> {
 }
 
 fn main() {
-    let opt = Opt::from_args();
+    let opt = Opt::parse();
     let mut v = opt.numbers.clone();
-    
-    for n in 1..=opt.iterations {
+
+    let mut n = 0;
+    let prec = (10.0_f64).powi(opt.precision as i32);
+    loop {
+        n += 1;
         v = f(&v);
         if opt.debug {
             println!("{}:Gmdn() = {:?}", n, v);
         }
+        if opt.iterations.map(|x| x <= n).unwrap_or_default()
+            || v.iter()
+                .map(|x| (x * prec).trunc())
+                .collect::<Vec<_>>()
+                .windows(2)
+                .all(|w| w[0] == w[1])
+        {
+            break;
+        }
     }
-    println!("Gmdn({:?}) = {:?}", opt.numbers, v);
+    if opt.verbose {
+        println!("Gmdn({:?}) iterations:{n} -> {:?}", opt.numbers, v);
+    }
+    println!("{:.*}", opt.precision as usize, v[0]);
 }
